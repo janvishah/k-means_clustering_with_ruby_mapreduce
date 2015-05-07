54f7ef2c14ca10989e32404a6758250296ca9f58
Janvi Shah <shahjanvi54@gmail.com>
2015-05-08 00:15:17 +0530

    Create map file which produce key-value pair

--- map.rb
+++ map.rb
@@ -1,32 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'csv'
-require 'gnuplot'
-
-# load csv and find data from it
-drugs = []
-accidents_due_to_drugs = []
-CSV.foreach("state.csv") do |row|
-  drugs.push(row[1].to_i)
-  accidents_due_to_drugs.push(row[2].to_i)
-end
-puts "Drugs"
-puts drugs
-puts "No of road accidents"
-puts accidents_due_to_drugs
-
-# scatter plot for graph
-outFname='drugs_vs_accidents.png'
-Gnuplot.open do |gp|
-  Gnuplot::Plot.new( gp ) do |plot|
-  	plot.output outFname
-		plot.terminal 'png'
-    plot.title  "Array Plot Example"
-    plot.xlabel "Drug"
-    plot.ylabel "No of road accident"    
-
-    plot.data << Gnuplot::DataSet.new( [drugs, accidents_due_to_drugs] ) do |ds|
-    ds.notitle
-    end
+key = nil
+STDIN.each do |line|
+  value = nil
+  line.split(',').each_with_index do |word,index|
+    if index==0 
+      key = word 
+    else
+      value == nil ? value = word : value = value + "," + word
+    end     
   end
+  puts key + "\t" + value 
 end
--- reduce.rb
+++ reduce.rb
@@ -1,19 +1,73 @@
-#!/usr/bin/ruby1.9.1
+#!/usr/bin/env ruby
 
-prev_key=nil
-init_val = 1
+require 'gnuplot'
 
+# load csv and find data from it
+drugs = []
+accidents_due_to_drugs = []
 STDIN.each do |line|
-key, value = line.split("\t")
-if prev_key != nil && prev_key != key
-puts prev_key + "\t" + init_val.to_s
-prev_key = key
-init_val = 1
-elsif prev_key == nil
-prev_key = key
-elsif prev_key == key
-init_val = init_val + value.to_i
-end
+	key, value = line.split("\t")	
+	value.split(',').each_with_index do |word,index|
+  	drugs.push(word.to_i) if index == 0
+  	accidents_due_to_drugs.push(word.to_i) if index==1
+  end
 end
 
-puts prev_key + "\t" + init_val.to_s
+# scatter plot for graph
+outFname='drugs_vs_accidents.png'
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new( gp ) do |plot|
+  	plot.output outFname
+		plot.terminal 'png'
+    plot.title  "Array Plot Example"
+    plot.xlabel "Drug"
+    plot.ylabel "No of road accident"    
+
+    plot.data << Gnuplot::DataSet.new( [drugs, accidents_due_to_drugs] ) do |ds|
+    ds.notitle
+    end
+  end
+end
+
+center_x = [drugs[0],drugs[1],drugs[2]]
+center_y = [accidents_due_to_drugs[0],accidents_due_to_drugs[1],accidents_due_to_drugs[2]]
+
+cluster1_x = [drugs[0]]
+cluster2_x = [drugs[1]]
+cluster3_x = [drugs[2]]
+cluster1_y = [accidents_due_to_drugs[0]]
+cluster2_y = [accidents_due_to_drugs[1]]
+cluster3_y = [accidents_due_to_drugs[2]]
+
+drugs.each_with_index do |drug,key|
+  
+  distance_from_center1 = (center_x[0]-drug)*(center_x[0]-drug) + (center_y[0]-accidents_due_to_drugs[key])*(center_y[0]-accidents_due_to_drugs[key])
+  distance_from_center2 = (center_x[1]-drug)*(center_x[1]-drug) + (center_y[1]-accidents_due_to_drugs[key])*(center_y[1]-accidents_due_to_drugs[key])
+  distance_from_center3 = (center_x[2]-drug)*(center_x[2]-drug) + (center_y[2]-accidents_due_to_drugs[key])*(center_y[2]-accidents_due_to_drugs[key])
+
+  min_distance = distance_from_center1
+  min_distance = distance_from_center2 if min_distance >= distance_from_center2    
+  min_distance = distance_from_center3 if min_distance >= distance_from_center3
+    
+  next if min_distance == 0
+
+  if min_distance == distance_from_center1
+    puts key
+    puts "min distance is from cluster1"
+    cluster1_x.push(drug) 
+    cluster1_y.push(accidents_due_to_drugs[key]) 
+  end
+  if min_distance == distance_from_center2
+    puts key
+    puts "min distance is from cluster2"
+    cluster2_x.push(drug) 
+    cluster2_y.push(accidents_due_to_drugs[key]) 
+  end
+  if min_distance == distance_from_center3
+    puts key
+    puts "min distance is from cluster3"
+    cluster3_x.push(drug) 
+    cluster3_y.push(accidents_due_to_drugs[key]) 
+  end
+end
+
